d6c7d6153c2a6ca5d0ce7bb9b4e8f8e2a00bc33c
surajlunthi <50191829+surajlunthi@users.noreply.github.com>
2025-02-06 00:32:22 +0530

    marshal

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+First string
+}
+
+func main(){
+
+	p1 := person{
+		First : "jenny",
+	}
+	p2 := person{
+		First:"James",
+	}
+
+	xp := []person{p1, p2}
+
+	bs,err := json.Marshal(xp)
+
+	if err!=nil{
+		log.Panic(err)
+	}
+	fmt.Println("byte array",bs)
+	fmt.Println(string(bs))
+
+
+}
