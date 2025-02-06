18ab578679be0ea17c5fe4f431c1465a5c2986dd
surajlunthi <50191829+surajlunthi@users.noreply.github.com>
2025-02-06 09:56:18 +0530

    encryption learn

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module surajlunthi.com/golang-projects
 
 go 1.21.3
+
+require golang.org/x/crypto v0.32.0 // indirect
--- main.go
+++ main.go
@@ -2,19 +2,54 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type person struct {
 	First string
 }
 
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return nil, err
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashPassword []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPassword, []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
 func main() {
 
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8081", nil)
+	pass := "123456789suraj"
+
+	v, err := hashPassword(pass)
+	if err != nil {
+		fmt.Println("Error is ", err)
+	}
+
+	err = comparePassword(pass, v)
+	if err != nil {
+		fmt.Println("Password comparison error:", err)
+	} else {
+		fmt.Println("Password matched successfully!")
+	}
+
+	// http.HandleFunc("/encode", foo)
+	// http.HandleFunc("/decode", bar)
+	// http.ListenAndServe(":8081", nil)
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
